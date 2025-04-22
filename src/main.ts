e86a5e140b2e0763d55e93b877ceb947a83ee619
João Pedro de Salles Braga <jpsbdbz@gmail.com>
2025-04-22 17:23:02 -0300

    feat(documentação): :memo: Criando documentação swagger para a API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Mentora API')
+    .setDescription(
+      'API para acesso aos dados da aplicação Mentora.\nConsulte as informaçÕes dos mentores e seus dados para explorar todas as possibilidades e aprender muito mais!',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
